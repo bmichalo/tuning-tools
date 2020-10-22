bcd2a020675b133ef75faf5627534f374b529715
root <root@perf19.perf.lab.eng.bos.redhat.com>
2020-10-22 12:13:05 -0400

    Fixing more formatting and iteration variables

--- run_lmbench_test.sh
+++ run_lmbench_test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#parallelism=(1 8 16 32)
-#message_size=(8192 16384 65536)
-parallelism=(1 8)
-message_size=(8192 16384)
+parallelism=(1 8 16 32)
+message_size=(8192 16384 65536)
 prior_best_result_Gbps=0
 initialize_prior_best_result_Gbps=1
 
@@ -10,13 +8,17 @@ for i in "${message_size[@]}"
 do
 	for j in "${parallelism[@]}"
 	do
-		for k in {1..1}
+		for k in {1..3}
 		do
-			echo "bw_tcp -m $i -P $j 10.0.1.21:  " | tr -d '\n';bw_tcp -m $i -P $j 10.0.1.21 &> see2.txt
+			#echo "bw_tcp -m $i -P $j 10.0.1.21:  " | tr -d '\n'
+			printf "%-10s%5d%3s%3d%s" "bw_tcp -m" $i "-P" $j "  10.0.1.21:  "
+			bw_tcp -m $i -P $j 10.0.1.21 &> see2.txt
 			answer_MBps=$(awk '{print $2}' see2.txt)
-			echo "$answer_MBps MBps ..... " | tr -d '\n'
+			#echo "$answer_MBps MBps ..... " | tr -d '\n'
+			printf "%12.4f%s" $answer_MBps " MBps ..... "
 			answer_Gbps=$(echo "scale=4;${answer_MBps} * 8/10^3" | bc -l)
-			echo "$answer_Gbps Gbps"
+			#echo "$answer_Gbps Gbps"
+			printf "%9.4f%s\n" $answer_Gbps " Gbps"
 
 			if [ "$initialize_prior_best_result_Gbps" -eq 1 ]; then
 				prior_best_result_Gbps=$answer_Gbps
@@ -48,7 +50,7 @@ prior_worst_result_Gbps=0
 worst_results=0
 better_results=0
 
-for k in {1..3}
+for k in {1..5}
 do
 	echo "bw_tcp -m $best_message_size -P $best_parallelism 10.0.1.21:  " | tr -d '\n';bw_tcp -m $best_message_size -P $best_parallelism 10.0.1.21 &> see2.txt
 	answer_MBps=$(awk '{print $2}' see2.txt)
